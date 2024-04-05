f2ca88f0ee225e977511db30bd045679ce8c3973
Nicolas Raulin <nicolas.raulin@epfl.ch>
2024-04-05 18:04:06 +0200

    dedup profiling files

--- pkg/cover/cover.profiling.go
+++ pkg/cover/cover.profiling.go
@@ -1,4 +1,4 @@
-//go:build !profiling
+//go:build profiling
 
 // Copyright 2015 syzkaller project authors. All rights reserved.
 // Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
